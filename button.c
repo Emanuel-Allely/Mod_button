52a8b183884ae6682d750ebc2fd11000553705d3
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-06-07 12:51:27 +0200

    hide robus

--- button.c
+++ button.c
@@ -1,7 +1,4 @@
-#include "main.h"
-#include "robus.h"
-#include "luos.h"
-#include "l0.h"
+#include "gpio.h"
 #include "button.h"
 
 static vm_t *vm_pointer;
@@ -9,39 +6,26 @@ static volatile msg_t pub_msg;
 static volatile int pub = LUOS_PROTOCOL_NB;
 
 void rx_btn_cb(vm_t *vm, msg_t *msg) {
-	if (msg->header.cmd == ASK_PUB_CMD) {
-		// fill the message infos
-		pub_msg.header.cmd = IO_STATE;
-		pub_msg.header.target_mode = ID;
-		pub_msg.header.target = msg->header.source;
-		pub_msg.header.size = sizeof(char);
-		pub_msg.data[0] = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
-		pub = ASK_PUB_CMD;
-		return;
-	}
-	// Luos message management
-   	int pub_type = luos_msg_handler(vm, msg, &pub_msg);
-	if (pub_type != LUOS_PROTOCOL_NB) {
-		vm_pointer = vm;
-		pub = pub_type;
-		return;
-	}
-	// L0 message management
-    pub_type = l0_msg_handler(vm, msg, &pub_msg);
-	if (pub_type != LUOS_PROTOCOL_NB & pub_type != L0_LED) {
-		vm_pointer = vm;
-		pub = pub_type;
-		return;
-	}
+    if (msg->header.cmd == ASK_PUB_CMD) {
+        // fill the message infos
+        pub_msg.header.cmd = IO_STATE;
+        pub_msg.header.target_mode = ID;
+        pub_msg.header.target = msg->header.source;
+        pub_msg.header.size = sizeof(char);
+        pub_msg.data[0] = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
+        pub = ASK_PUB_CMD;
+        vm_pointer = vm;
+        return;
+    }
 }
 
 void button_init(void) {
-  robus_module_create(rx_btn_cb, BUTTON, "button_mod");
+    luos_module_create(rx_btn_cb, BUTTON, "button_mod");
 }
 
 void button_loop(void) {
-  if (pub != LUOS_PROTOCOL_NB) {
-    robus_send(vm_pointer, &pub_msg);
-    pub = LUOS_PROTOCOL_NB;
-  }
+    if (pub != LUOS_PROTOCOL_NB) {
+        luos_send(vm_pointer, &pub_msg);
+        pub = LUOS_PROTOCOL_NB;
+    }
 }
