a079eaf6f127acf178f043fd48c56f946e44a789
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-09-04 09:55:10 +0200

    Switch to no real time

--- button.c
+++ button.c
@@ -1,31 +1,25 @@
 #include "gpio.h"
 #include "button.h"
 
-static vm_t *vm_pointer;
-static volatile msg_t pub_msg;
-static volatile int pub = LUOS_PROTOCOL_NB;
-
-void rx_btn_cb(vm_t *vm, msg_t *msg) {
+void rx_btn_cb(module_t *module, msg_t *msg) {
     if (msg->header.cmd == ASK_PUB_CMD) {
         // fill the message infos
+        msg_t pub_msg;
         pub_msg.header.cmd = IO_STATE;
         pub_msg.header.target_mode = ID;
         pub_msg.header.target = msg->header.source;
         pub_msg.header.size = sizeof(char);
         pub_msg.data[0] = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
-        pub = ASK_PUB_CMD;
-        vm_pointer = vm;
+        luos_send(module, &pub_msg);
         return;
     }
 }
 
 void button_init(void) {
-    luos_module_create(rx_btn_cb, STATE_MOD, "button_mod");
+    module_t* module = luos_module_create(rx_btn_cb, STATE_MOD, "button_mod");
+
 }
 
 void button_loop(void) {
-    if (pub != LUOS_PROTOCOL_NB) {
-        luos_send(vm_pointer, &pub_msg);
-        pub = LUOS_PROTOCOL_NB;
-    }
+
 }
